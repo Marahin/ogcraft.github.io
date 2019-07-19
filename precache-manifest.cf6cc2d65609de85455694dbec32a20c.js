1acf0695007163a49bea32883e3f17b39b64f558
marahin <me@marahin.pl>
2019-07-19 21:20:40 +0200

    move to react build

---
+++ precache-manifest.cf6cc2d65609de85455694dbec32a20c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53077333b29121b669cdfef6b555af9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48186f1fa72da544bbef",
+    "url": "/static/css/main.81d1986f.chunk.css"
+  },
+  {
+    "revision": "48d3019fe7d9ce5b85c3",
+    "url": "/static/js/2.37ee7778.chunk.js"
+  },
+  {
+    "revision": "48186f1fa72da544bbef",
+    "url": "/static/js/main.fe05d3fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "92bca4df868aa72638c53a2cd916a0d7",
+    "url": "/static/media/discord.92bca4df.png"
+  },
+  {
+    "revision": "c9c6d4145dbb1860598bee4e9ed27534",
+    "url": "/static/media/fb.c9c6d414.png"
+  },
+  {
+    "revision": "4b1993d7140817c035b18474d2764787",
+    "url": "/static/media/minecraft.4b1993d7.jpg"
+  },
+  {
+    "revision": "ba51458ffa4cc32a57fed750871ae94f",
+    "url": "/static/media/suit-steve.ba51458f.png"
+  },
+  {
+    "revision": "591a9ad38819431ca2e8cdc1db98c62c",
+    "url": "/static/media/xd.591a9ad3.png"
+  }
+]);
